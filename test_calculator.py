c1745c9fdeffc0f92caa94e94d7acdaaeb54f909
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-29 15:37:15 +0530

    Pytest calculator example.

--- test_calculator.py
+++ test_calculator.py
@@ -1,25 +1,30 @@
 import calculator
+import pytest
 
-def test_add():
-    x=10
-    y=20
-    r=calculator.add(x, y)
-    assert x+y == r
+@pytest.mark.parametrize("a,b,c",[(3,2,5),(4,2,6),(5,5,10),(5,5,9)])
+def test_add(a,b,c):
+    # x=10
+    # y=20
+    r=calculator.add(a,b)
+    assert a+b == c
 
-def test_sub():
-    x=10
-    y=20
-    r=calculator.sub(x, y)
-    assert x-y == r
+@pytest.mark.parametrize("a,b,c",[(3,2,1),(4,2,2),(5,5,0),(5,5,9)])
+def test_sub(a,b,c):
+    # x=10
+    # y=20
+    r=calculator.sub(a, b)
+    assert a-b == c
 
-def test_mult():
-    x=10
+@pytest.mark.parametrize("a,b,c",[(3,2,6),(4,2,8),(5,5,25),(5,5,9)])
+def test_mult(a,b,c):
+    # x=10
     y=20
-    r=calculator.mult(x, y)
-    assert x*y == r
+    r=calculator.mult(a, b)
+    assert a*b == c
 
-def test_div():
-    x=10
-    y=20
-    r=calculator.div(x, y)
-    assert x/y == r
+@pytest.mark.parametrize("a,b,c",[(3,2,1.5),(4,2,2),(5,5,1),(5,5,9)])
+def test_div(a,b,c):
+    # x=10
+    # y=20
+    r=calculator.div(a, b)
+    assert a/b == c
