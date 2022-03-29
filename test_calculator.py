edace7a89d8face8d6f3faab62d6b6b62ee10421
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-29 15:55:05 +0530

    Pytest features experienced.

--- test_calculator.py
+++ test_calculator.py
@@ -1,21 +1,26 @@
 import calculator
 import pytest
 
+@pytest.mark.skip #command to skip the particular test case
+@pytest.mark.xfail #command for expected error lines
 @pytest.mark.parametrize("a,b,c",[(3,2,5),(4,2,6),(5,5,10),(5,5,9)])
 def test_add(a,b,c):
     r=calculator.add(a,b)
     assert r == c
 
+@pytest.mark.xfail
 @pytest.mark.parametrize("a,b,c",[(3,2,1),(4,2,2),(5,5,0),(5,5,9)])
 def test_sub(a,b,c):
     r=calculator.sub(a, b)
     assert r == c
 
+@pytest.mark.xfail
 @pytest.mark.parametrize("a,b,c",[(3,2,6),(4,2,8),(5,5,25),(5,5,9)])
 def test_mult(a,b,c):
     r=calculator.mult(a, b)
     assert r == c
 
+@pytest.mark.xfail
 @pytest.mark.parametrize("a,b,c",[(3,2,1.5),(4,2,2),(5,5,1),(5,5,9)])
 def test_div(a,b,c):
     r=calculator.div(a, b)
